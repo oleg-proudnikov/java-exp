47c026eb3f6bbbeec2a66dc0c605155cb9be97c9
oleg-targetb <oleg.proudnikov@targetb.io>
2021-04-23 15:08:31 +0100

    FizzBuzz

--- src/main/java/dev/intx/interview/FizzBuzz.java
+++ src/main/java/dev/intx/interview/FizzBuzz.java
@@ -2,15 +2,14 @@ package dev.intx.interview;
 
 public class FizzBuzz {
 
-    public String evaluate(int n) {
+    public static String evaluate(int n) {
         return n % 15 == 0 ? "FizzBuzz" : (n % 3 == 0 ? "Fizz" : (n % 5 == 0 ? "Buzz" : "" + n));
     }
 
     public static void main(String[] args) {
-        System.out.println(new FizzBuzz().evaluate(1));
-        System.out.println(new FizzBuzz().evaluate(3));
-        System.out.println(new FizzBuzz().evaluate(5));
-        System.out.println(new FizzBuzz().evaluate(15));
+        for (int i : new int[]{1, 3, 5, 15}) {
+            System.out.println(evaluate(i));
+        }
     }
 
 }
